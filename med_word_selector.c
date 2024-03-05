87b77d2cd420a0a1aaff4173776a8895de1547c1
nguyen9234 <162350164+nguyen9234@users.noreply.github.com>
2024-03-05 10:01:46 -0700

    Create med_word_selector.c

---
+++ med_word_selector.c
@@ -0,0 +1,46 @@
+// Function puts in med words into the file 
+// Uses random number generator to randomly select word 
+// Returns word
+char* med_word()
+{
+    char* word = malloc(15);
+    
+    /* Puts words into a txt file */
+    FILE *ptr = fopen("med_words.txt", "w");
+    char word_1[15] = "dilemma\n";
+    char word_2[15] = "soldier\n";
+    char word_3[15] = "seminar\n";
+    char word_4[15] = "contact\n";
+    char word_5[15] = "climate\n";
+    fputs(word_1, ptr);
+    fputs(word_2, ptr);
+    fputs(word_3, ptr);
+    fputs(word_4, ptr);
+    fputs(word_5, ptr);
+    fclose(ptr);
+    
+    int num, i;
+    num = rand()%5+1;
+    
+    ptr = fopen("med_words.txt", "r");
+    
+    /* Randomly selects word and returns it*/
+    for(i=0;i<num;i++)
+    {
+        fgets(word, 15, ptr);
+    }
+    
+    return word;
+    
+}
+
+/* Use for checking  
+int main()
+{
+    srand(time(NULL));
+    char* word;
+    word = med_word();
+    printf("%s", word);
+    return 0;
+}
+*/
