4eaf2a43483098d443c31df1f8205a63c4c324e7
nguyen9234 <162350164+nguyen9234@users.noreply.github.com>
2024-03-18 15:47:13 -0700

    Update hard_word_selector.c

--- hard_word_selector.c
+++ hard_word_selector.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // Function puts in med words into the file 
 // Uses random number generator to randomly select word 
 // Returns word
