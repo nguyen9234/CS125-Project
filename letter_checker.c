6e15f417d9f0286a59867a47be025cd8cad4899d
nguyen9234 <162350164+nguyen9234@users.noreply.github.com>
2024-03-21 10:24:19 -0700

    Update letter_checker.c

--- letter_checker.c
+++ letter_checker.c
@@ -45,48 +45,40 @@ void letter_checker(char a, int b)
     }  
 }
 
-/*main
-int main()
+//New version
+#include <stdio.h>
+#include <stdlib.h>
+
+char* easy_word();
+char* med_word();
+char* hard_word();
+
+//User types in a number to choose level 
+//Function for selected level will be called
+//Returns a random word
+
+char* choose_level(int a)
 {
-    int count=0, i;
-    char null_word[10] = "-----";
-    char guess;
-    
-    srand(time(NULL));
-    char* word;
-    word = easy_word();
-    printf("%s\n", word);
-    int c = strcmp(null_word, word);
-    printf("%d\n", c);
-    
-    while ((strcmp(null_word, word) < -10) && (count < 6))
-    {
-        printf("Guess a letter: ");
-        scanf(" %c", &guess);
-        guess = tolower(guess);
-
-        int found = 0;
-       
-        for(i=0; i<5; i++)
-        {
-            if (guess == word[i])
-            {
-                null_word[i] = word[i]; 
-                found++;
-            }
-            
-        }
-        
-        if (!found)
-        {   
-            printf("Error\n");
-            count++;
-        }
-        
-        printf("%d\n", c);
-        printf("%s\n", null_word);
-       
-    }
-
-    return 0;
-}*/
+  char* word;
+  int option;
+  while((a != 1) && (a != 2) && (a != 3))
+  {
+      printf("Invalid option\n");
+      printf("What level would you like to play?");
+      scanf("%d", &a);
+      
+      if ((a == 1) || (a == 2) || (a == 3))
+        break;
+  }
+  
+  if ((a == 1) || (option == 1))
+      word = easy_word();
+  
+  else if ((a == 2) || (option == 2))
+      word = med_word();
+  
+  else if ((a == 3) || (option == 3))
+      word = hard_word();
+      
+  return word;
+}      
