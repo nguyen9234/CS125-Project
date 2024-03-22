79d5bf714f1dc2df44986c22aa1e294a97b52be4
nguyen9234 <162350164+nguyen9234@users.noreply.github.com>
2024-03-22 15:31:51 -0700

    Create nhiMainCodeForTest.c

---
+++ nhiMainCodeForTest.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+
+char* easy_word();
+char* med_word();
+char* hard_word();
+char* choose_level(int a);
+char* dash(char *a);
+char* letter_checker(char a, int b);
+
+int main()
+{
+    srand(time(NULL));
+    const char* word;
+    int option;
+    char guess;
+    
+    printf("What level would you like to play?");
+    scanf("%d", &option);
+    
+    
+
+    letter_checker(guess, option);
+    return 0;
+}
