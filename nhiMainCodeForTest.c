df485370f301f39ab8b3245cd68630aeff7c7035
nguyen9234 <162350164+nguyen9234@users.noreply.github.com>
2024-03-27 15:44:56 -0700

    Update nhiMainCodeForTest.c

--- nhiMainCodeForTest.c
+++ nhiMainCodeForTest.c
@@ -13,12 +13,14 @@ char* letter_checker(int b);
 void calltitle();
 void print_hangmans_hang(int x);
 void hangman_hang(int x);
+void success();
+void failure();
 
 int main()
 {
     srand(time(NULL));
     const char* word;
-    int option;
+    int option, win_lose;
     char guess;
     
     calltitle();
@@ -26,7 +28,11 @@ int main()
     
     printf("What level would you like to play? (1=easy, 2=medium, 3=hard)");
     scanf("%d", &option);
-    
+    //win_lose = letter_checker(option);
     letter_checker(option);
+    //if win_lose == 1
+    //   call success
+    //else if win_lose == 0
+    //   call lose
     return 0;
 }
