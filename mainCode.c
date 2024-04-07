5ae54f9a2e046113be3c0df0ba63bee20389e8a7
nguyen9234 <162350164+nguyen9234@users.noreply.github.com>
2024-04-07 10:09:31 -0700

    Update and rename MainCode.c to mainCode.c

--- mainCode.c
+++ mainCode.c
@@ -26,10 +26,12 @@ void failure();
 int main()
 {
     srand(time(NULL));
+    //Variables
     const char* word;
     int option, win_lose;
     char guess;
-    
+
+    //Game start
     calltitle();
     printf("\n\n");
     printf("    'Rules of the game'\n");
@@ -37,7 +39,11 @@ int main()
     printf("2) You have 6 chances to guess correctly\n");
     printf("3) Have FUN :] \n\n");
     printf("What level would you like to play? (1=easy, 2=medium, 3=hard)");
+
+    //Start of game
     win_lose = letter_checker(option);
+
+    //Print win or lose statement
     if (win_lose ==1)
     {
        success();
